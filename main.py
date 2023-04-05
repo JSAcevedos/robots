e487d172b6fed4b1997668175a577d6131d29cf1
Juan Acevedo <juan.acevedo9805@gmail.com>
2023-04-04 23:45:02 -0500

    New solution for the problem using a stack implementation

--- main.py
+++ main.py
@@ -1,34 +1,72 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.before = None
 
-def printArray(array):
-    for i in array:
-        if i == array[-1]:
-            print(i, end="")
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head is None
+
+    def pop(self):
+        if self.isEmpty():
+            print('Empty stack')
         else:
-            print(i, end=" ")
+            temp = self.head.data
+            self.head = self.head.before
+            return temp
+    
+    def push(self, data):
+        node = Node(data)
+        node.before = self.head
+        self.head = node
+    
+    def print(self):
+        if self.head is None:
+            print("Empty stack")
+        else:
+            current_node = self.head
+            while current_node is not None:
+                if current_node.before is None:
+                    print(current_node.data, end="")
+                else:
+                    print(current_node.data, end=" ")
+                current_node = current_node.before
 
-list = input().split()
-left = []
-right = []
+data = input().split()
+left = Stack()
+right = Stack() 
+result = Stack()
+temp = Stack()
 
-for i in list:
-    if int(i) > 0:
-        left.append(int(i))
-    elif int(i) < 0:
-        right.append(int(i))
+for i in data:
+    result.push(int(i))
 
-while len(left) != 0 and len(right) != 0:
-    if left[-1] > abs(right[0]):
-        right.pop(0)
-    elif left[-1] < abs(right[0]):
+while not result.isEmpty():
+    if result.head.data > 0:
+        temp.push(result.pop())
+    elif result.head.data < 0:
+        right.push(result.pop())
+
+while not temp.isEmpty():
+        left.push(temp.pop())
+
+while not left.isEmpty() and not right.isEmpty():
+    if left.head.data > abs(right.head.data):
+        right.pop()
+    elif left.head.data < abs(right.head.data):
         left.pop()
     else:
-        right.pop(0)
         left.pop()
+        right.pop()
 
-if len(left) == 0 and len(right) != 0:
-    printArray(right)
-elif len(left) != 0 and len(right) == 0: 
-    printArray(left)
+if left.isEmpty() and not right.isEmpty():
+    right.print()
+elif right.isEmpty() and not left.isEmpty():
+    while not left.isEmpty():
+        right.push(left.pop())
+    right.print()
 else:
     print("No quedaron robots!", end="")
-
